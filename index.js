9eef05b29a59d3c4240cdd9bd3e13422143e40b9
Andy Hitchings <andyfmhitchings@gmail.com>
2018-05-19 18:15:52 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+var bgImageArray = ["hero1.jpg"],
+base = "images/",
+secs = 8;
+bgImageArray.forEach(function(img){
+    new Image().src = base + img; 
+    // caches images, avoiding white flash between background replacements
+});
+
+function backgroundSequence() {
+	window.clearTimeout();
+	var k = 0;
+	for (i = 0; i < bgImageArray.length; i++) {
+		setTimeout(function(){ 
+			document.documentElement.style.background = "url(" + base + bgImageArray[k] + ") no-repeat center center fixed";
+			document.documentElement.style.backgroundSize ="cover";
+		if ((k + 1) === bgImageArray.length) { setTimeout(function() { backgroundSequence() }, (secs * 1000))} else { k++; }			
+		}, (secs * 1000) * i)	
+	}
+}
+backgroundSequence();
+
